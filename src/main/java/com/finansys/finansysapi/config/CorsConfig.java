c46e0997592cac0ffd3ea9e3fe120e45763dbd95
marcio <margeodev@gmail.com>
2025-09-17 14:43:36 -0300

    Criação da configuração do cors

---
+++ src/main/java/com/finansys/finansysapi/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.finansys.finansysapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://finansys-webapp-production.up.railway.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("Content-Type", "email", "password")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
